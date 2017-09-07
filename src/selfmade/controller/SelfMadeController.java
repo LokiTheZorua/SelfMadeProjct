a59b0a23bbb4c3287f0f1f7ab4ad3f273e889613
LokiTheZorua <31518758+LokiTheZorua@users.noreply.github.com>
2017-09-07 13:53:20 -0600

    Added notes

--- src/selfmade/controller/SelfMadeController.java
+++ src/selfmade/controller/SelfMadeController.java
@@ -4,6 +4,6 @@ public class SelfMadeController
 {
 	public void start()
 	{
-		System.out.println("I don't know how I am doing this but I am.");
+		System.out.println("I don't know how I am doing this but I am.");	//I want to change the color of the text
 	}
 }
